a5d26e9e35eb93fcf883e0247ba06adc7e9bf4fc
Lucas Miguins <lucasmiguins@gmail.com>
2019-05-11 17:11:12 -0300

    Salvar dados do here a cada 1hr

--- codigo_fonte/back_rails/app/controllers/application_controller.rb
+++ codigo_fonte/back_rails/app/controllers/application_controller.rb
@@ -19,30 +19,33 @@ class ApplicationController < ActionController::API
 
   public
   def salvar_json
-    response = RestClient::Request.execute(method: :get, url: 'https://servicehere.herokuapp.com/?key=jamalgay24', timeout: 10)
-	  parsed_json = ActiveSupport::JSON.decode(response.body)
+      900.times do
+        response = RestClient::Request.execute(method: :get, url: 'https://servicehere.herokuapp.com/?key=jamalgay24', timeout: 10)
+        parsed_json = ActiveSupport::JSON.decode(response.body)
 
-    for rua in parsed_json["dados"]["ruas"]["cruzamentos"]
-      # site = Cruzamento::new()
-      # site.nomeRuaPrincipal = rua["nomeRuaPrincipal"]
-      # site.idRua = rua["id"]
-      # site.tempoDaPrevisao = rua["tempoDaPrevisao"]
-      
-      site = Cruzamento.where({
-        :idRua => rua["id"]
-      }).first_or_create(nomeRuaPrincipal: rua["nomeRuaPrincipal"], tempoDaPrevisao: rua["tempoDaPrevisao"])
+        for rua in parsed_json["dados"]["ruas"]["cruzamentos"]
+          # site = Cruzamento::new()
+          # site.nomeRuaPrincipal = rua["nomeRuaPrincipal"]
+          # site.idRua = rua["id"]
+          # site.tempoDaPrevisao = rua["tempoDaPrevisao"]
+          
+          site = Cruzamento.where({
+            :idRua => rua["id"]
+          }).first_or_create(nomeRuaPrincipal: rua["nomeRuaPrincipal"], tempoDaPrevisao: rua["tempoDaPrevisao"])
 
-      for transversal in rua["ruasTransversais"]
-        t = RuaTransversal::new()
-        t.nomeRuaTransversal = transversal["nomeRuaTransversal"]
-        t.pontosDeEncontro = transversal["pontosDeEncontro"]
-        t.velocidadeDeFluxoAtual = transversal["fluxoAtual"][0]["velocidadeDeFluxoAtual"]
-        t.velocidadeEmFluxoLivre = transversal["fluxoAtual"][0]["velocidadeEmFluxoLivre"]
-        t.nivelDeTrafego = transversal["fluxoAtual"][0]["nivelDeTrafego"]
-        t.cruzamentoId = site.idRua
-        
-        t.save
-      end
-    end
+          for transversal in rua["ruasTransversais"]
+            t = RuaTransversal::new()
+            t.nomeRuaTransversal = transversal["nomeRuaTransversal"]
+            t.pontosDeEncontro = transversal["pontosDeEncontro"]
+            t.velocidadeDeFluxoAtual = transversal["fluxoAtual"][0]["velocidadeDeFluxoAtual"]
+            t.velocidadeEmFluxoLivre = transversal["fluxoAtual"][0]["velocidadeEmFluxoLivre"]
+            t.nivelDeTrafego = transversal["fluxoAtual"][0]["nivelDeTrafego"]
+            t.cruzamentoId = site.idRua
+            
+            t.save
+          end
+        end
+      sleep(1.hours)
+    end # times fim
   end
 end
